99be01677d9d9de64ad7a3ee61c046f2e75bd3b9
LINNER33 <444655279@qq.com>
2026-06-15 07:09:46 +0900

    添加charset=UTF-8到脚本标签修复编码

---
+++ _chinese_test.js
@@ -0,0 +1,2 @@
+﻿const test = "唐代藩镇制度";
+console.log(test);
---
+++ _fix_charset.js
@@ -0,0 +1,22 @@
+﻿const fs = require("fs");
+const pages = ["index.html","articles.html","books.html","snippets.html","biographies.html","resources.html","downloads.html","research.html","article.html"];
+let n = 0;
+pages.forEach(p => {
+  try {
+    let c = fs.readFileSync("C:\\Users\\华为\\Documents\\historyramble\\" + p, "utf8");
+    let o = c;
+    c = c.replace(/(<script src="js\/data\.js")>/g, '$1 charset="UTF-8">');
+    if (c !== o) { fs.writeFileSync("C:\\Users\\华为\\Documents\\historyramble\\" + p, c, "utf8"); n++; }
+  } catch(e) {}
+});
+console.log("Fixed " + n + " pages");
+// Also fix main.js
+pages.forEach(p => {
+  try {
+    let c = fs.readFileSync("C:\\Users\\华为\\Documents\\historyramble\\" + p, "utf8");
+    let o = c;
+    c = c.replace(/(<script src="js\/main\.js")>/g, '$1 charset="UTF-8">');
+    if (c !== o) { fs.writeFileSync("C:\\Users\\华为\\Documents\\historyramble\\" + p, c, "utf8"); }
+  } catch(e) {}
+});
+console.log("Done fixing main.js too");
